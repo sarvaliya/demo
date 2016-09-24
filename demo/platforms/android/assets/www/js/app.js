f0d03effe98a2b4bdf5abe1c9fc77a85f47772c9
sarvaliya <vipulpatel1533@gmil.com>
2016-09-24 15:52:23 +0530

    add demo

---
+++ demo/platforms/android/assets/www/js/app.js
@@ -0,0 +1,94 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+.state('app.captureimg', {
+      url: '/captureimg',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/captureimg.html'
+
+        }
+      }
+    })
+.state('app.contactus', {
+      url: '/contactus',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/contactus.html'
+
+        }
+      }
+    })
+.state('app.feedback', {
+      url: '/feedback',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/feedback.html'
+
+        }
+      }
+    })
+.state('app.logout', {
+      url: '/logout',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/logout.html'
+
+        }
+      }
+    })
+
+    .state('app.profile', {
+      url: '/profiles',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/profiles.html',
+          controller: 'ProfilesCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/profiles/:profileId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profile.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/profiles');
+
+})
+;
